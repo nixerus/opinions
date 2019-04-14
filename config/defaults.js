c75f67aeac27c094a4ce040c3ee1b77d9f22c247
Ben Houghton <bennixerus@gmail.com>
2019-04-14 02:52:05 +0100

    Change from object to array

---
+++ config/defaults.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "Ben Nixerus",
+    imageUrl: "https://i.imgur.com/ipxtyGG.png",
+    bio: "Hio! I'm Ben, a gay developer from England! I'm single ;)",
+    age: 15,
+    facts: [
+        "I love bananas",
+        "I play Minecraft"
+    ]
+}
